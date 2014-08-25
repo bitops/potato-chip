e2871498fd6442d8105d0576023d349a05456ebf
Sebastian Wittenkamp <sebastian@bitops.io>
2014-08-24 18:20:07 -0700

    Add POST endpoint

--- potato_chip.rb
+++ potato_chip.rb
@@ -5,5 +5,13 @@ get '/' do
 end
 
 post '/chips' do
-  
+  if params["potato"] == "chip"
+    "All your potato chips are belong to us."
+  else
+    if params["potato"] != "" && !params["potato"].nil?
+      "Somebody set us up the chip."
+    else
+      "Take off every chip for great justice."
+    end
+  end
 end
