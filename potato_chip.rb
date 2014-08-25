90f55738de8ea2c720e2f62bbf9157d7a9d10bd1
Sebastian Wittenkamp <sebastian@bitops.io>
2014-08-24 21:41:35 -0700

    JSON Variation

--- potato_chip.rb
+++ potato_chip.rb
@@ -1,17 +1,23 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   "OK"
 end
 
 post '/chips' do
-  if params["potato"] == "chip"
-    "All your potato chips are belong to us."
+  content_type :json
+
+  request.body.rewind
+  json_data = JSON.parse(request.body.read)
+
+  if json_data["potato"] == "chip"
+    {"message" => "All your potato chips are belong to us."}.to_json
   else
-    if params["potato"] != "" && !params["potato"].nil?
-      "Somebody set us up the chip."
+    if json_data["potato"] != "" && !json_data["potato"].nil?
+      {"message" => "Somebody set us up the chip."}.to_json
     else
-      "Take off every chip for great justice."
+      {"message" => "Take off every chip for great justice."}.to_json
     end
   end
 end
